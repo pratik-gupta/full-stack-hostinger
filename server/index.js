db4aa22d6199b737305d6f2f2d24f8d3326f4c67
Pratik Gupta <pratik@Pratiks-MacBook-Air.local>
2026-06-26 11:35:01 +0530

    fix:cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 4000;
+
+dotenv.config({
+    path : `.env.${process.env.NODE_ENV}`
+});
+
+const host = process.env.host;
+
+console.log(`Host: ${host}`)
 
 app.use(express.json())
 app.use(cors(
